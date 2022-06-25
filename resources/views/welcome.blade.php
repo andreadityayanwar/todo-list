1979dfdc64436edf42b62cc688b549a0ea4b1eab
andreadityayanwar <andreadityayanwar@gmail.com>
2022-06-25 14:05:01 +0700

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
 
     <section>
         <div id="myDIV" class=" pb-8 bg-gray-500 max-w-xl mx-auto mt-10 rounded shadow">
-                <h1 class="text-3xl font-bold text-white text-center my-2">Todo List Apps</h1>
+                <h1 class="text-3xl font-bold text-white text-center my-2">TodoList Apps</h1>
                 <input type="text" id="myTask" class="w-80 p-1 ml-5 mb-1 shadow rounded" placeholder="Silahkan Tambahkan Disini">
                 <button type="submit" id="myPush" class="px-1 py-1 rounded text-white bg-blue-600 hover:bg-blue-800">Tambahkan</button>
                 
